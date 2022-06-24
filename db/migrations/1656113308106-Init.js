66dd5db08b29ecb5e9256ae43ccd8e4abfc39316
corlys <duscae047@gmail.com>
2022-06-25 06:29:05 +0700

    feat: delete tokenId and try unique Id for token

--- db/migrations/1656113308106-Init.js
+++ db/migrations/1656113308106-Init.js
@@ -1,5 +1,5 @@
-module.exports = class Init1656105948262 {
-  name = 'Init1656105948262'
+module.exports = class Init1656113308106 {
+  name = 'Init1656113308106'
 
   async up(db) {
     await db.query(`CREATE TABLE "owner" ("id" character varying NOT NULL, "balance" numeric, CONSTRAINT "PK_8e86b6b9f94aece7d12d465dc0c" PRIMARY KEY ("id"))`)
