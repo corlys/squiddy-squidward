4ad8f3a79ab20c8a65d6587cb92fbee84ca53c3e
corlys <duscae047@gmail.com>
2022-06-24 16:04:42 +0700

    feat: try to listen to degens as well

--- src/processor.ts
+++ src/processor.ts
@@ -4,8 +4,21 @@ import { lookupArchive } from "@subsquid/archive-registry";
 import { CHAIN_NODE, processTransfer } from "./contract";
 import { events } from "./abi/erc721";
 
-import { createAstarCatsContract, astarCatsContract, processAstarCatsTransfers } from "./helper/AstarCats"
-import { createJukiverseContract, jukiverseContract, processJukiverseTransfer } from "./helper/Jukiverse"
+import {
+  createAstarCatsContract,
+  astarCatsContract,
+  processAstarCatsTransfers,
+} from "./helper/AstarCats";
+import {
+  createJukiverseContract,
+  jukiverseContract,
+  processJukiverseTransfer,
+} from "./helper/Jukiverse";
+import {
+  createAstarDegenContract,
+  astarDegensContract,
+  processAstarDegenTransfers,
+} from "./helper/AstarDegens";
 
 const processor = new SubstrateEvmProcessor("astar-substrate");
 
@@ -16,13 +29,13 @@ const processor = new SubstrateEvmProcessor("astar-substrate");
  */
 
 // listen from astarDegens startBlock and up!
-// processor.setBlockRange({ from: 442693 })
+processor.setBlockRange({ from: 442693 });
 
 // listen from astarCats startBlock and up!
 // processor.setBlockRange({ from: 800854 })
 
 // listen from Jukiverse startBlock and up!
-processor.setBlockRange({ from: 1248161 })
+// processor.setBlockRange({ from: 1248161 });
 
 processor.setBatchSize(500);
 
@@ -33,26 +46,40 @@ processor.setDataSource({
 
 processor.setTypesBundle("astar");
 
+// Create Astar Degens contract Entitiy in their startBlock
+processor.addPreHook({ range: { from: 442693, to: 442693 } }, async (ctx) => {
+  await ctx.store.save(createAstarDegenContract());
+});
+
 // Create AstarCats contract Entity in their startBlock
-// processor.addPreHook({ range: { from: 800854, to: 800854 } }, async (ctx) => {
-//   await ctx.store.save(createAstarCatsContract());
-// });
+processor.addPreHook({ range: { from: 800854, to: 800854 } }, async (ctx) => {
+  await ctx.store.save(createAstarCatsContract());
+});
 
 // Create Jukiverse contract Entity in their startBlock
 processor.addPreHook({ range: { from: 1248161, to: 1248161 } }, async (ctx) => {
   await ctx.store.save(createJukiverseContract());
 });
 
-// Event listener for AstarCats Contract 
-// processor.addEvmLogHandler(
-//   astarCatsContract.address.toLowerCase(),
-//   {
-//     filter: [events["Transfer(address,address,uint256)"].topic],
-//   },
-//   processAstarCatsTransfers
-// );
+// Event listener for Astar Degens
+processor.addEvmLogHandler(
+  astarDegensContract.address.toLowerCase(),
+  {
+    filter: [events["Transfer(address,address,uint256)"].topic],
+  },
+  processAstarDegenTransfers
+);
 
-// Event listener for Jukiverse Contract 
+// Event listener for AstarCats Contract
+processor.addEvmLogHandler(
+  astarCatsContract.address.toLowerCase(),
+  {
+    filter: [events["Transfer(address,address,uint256)"].topic],
+  },
+  processAstarCatsTransfers
+);
+
+// Event listener for Jukiverse Contract
 processor.addEvmLogHandler(
   jukiverseContract.address.toLowerCase(),
   {
