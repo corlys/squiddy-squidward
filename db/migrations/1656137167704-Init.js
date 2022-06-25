55c7b4f6dd73b4afca1daa18157e22073dc059cd
corlys <duscae047@gmail.com>
2022-06-25 13:06:44 +0700

    feat: attempt to make the id unique the hundreth time

--- db/migrations/1656137167704-Init.js
+++ db/migrations/1656137167704-Init.js
@@ -1,5 +1,5 @@
-module.exports = class Init1656131022035 {
-  name = 'Init1656131022035'
+module.exports = class Init1656137167704 {
+  name = 'Init1656137167704'
 
   async up(db) {
     await db.query(`CREATE TABLE "owner" ("id" character varying NOT NULL, "balance" numeric, CONSTRAINT "PK_8e86b6b9f94aece7d12d465dc0c" PRIMARY KEY ("id"))`)
