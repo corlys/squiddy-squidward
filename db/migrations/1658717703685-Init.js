ed5fd5cfd28811ec10f5233a40fc6b593f0fa9cb
corlys <duscae047@gmail.com>
2022-07-25 09:56:30 +0700

    feat: test deploying

--- db/migrations/1658717703685-Init.js
+++ db/migrations/1658717703685-Init.js
@@ -1,5 +1,5 @@
-module.exports = class Init1658494524280 {
-  name = 'Init1658494524280'
+module.exports = class Init1658717703685 {
+  name = 'Init1658717703685'
 
   async up(db) {
     await db.query(`CREATE TABLE "owner" ("id" character varying NOT NULL, "balance" numeric, CONSTRAINT "PK_8e86b6b9f94aece7d12d465dc0c" PRIMARY KEY ("id"))`)
